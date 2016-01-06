4c4d6eac2ddbc584b2ef5e72608d4d4b9f46cdfc
Rui Peres <rui.peres@mailonline.co.uk>
2016-01-06 12:39:50 +0000

    Fixes according to swiftlint

--- OptionalExtensions/Source/OptionalExtensions.swift
+++ OptionalExtensions/Source/OptionalExtensions.swift
@@ -8,41 +8,41 @@
 
 
 public extension Optional {
-    
+
     func filter(@noescape predicate: Wrapped -> Bool) -> Optional {
-        
+
         return map(predicate) == .Some(true) ? self : .None
     }
-    
+
     func replaceNil(with replacement: Wrapped) -> Optional {
-        
+
         return self ?? replacement
     }
-    
+
     func then(@noescape f: Wrapped -> Void) {
-        
+
         if let wrapped = self { f(wrapped) }
     }
-    
+
     func onSome(@noescape f: Wrapped -> Void) -> Optional {
-        
+
         then(f)
         return self
     }
-    
+
     func onNone(@noescape f: Void -> Void) -> Optional {
-        
+
         if isNone { f() }
         return self
     }
-    
+
     var isSome: Bool {
-        
+
         return self != nil
     }
-    
+
     var isNone: Bool {
-        
+
         return !isSome
     }
 }
