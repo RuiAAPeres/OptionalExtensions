24739b0ae6cf014d300d33e82d256d23483a4b66
Rui Peres <rui.peres@mailonline.co.uk>
2016-09-22 11:49:25 +0100

    Swift3 (Xcode8 GM) fixes

--- OptionalExtensions/Source/OptionalExtensions.swift
+++ OptionalExtensions/Source/OptionalExtensions.swift
@@ -7,52 +7,42 @@
 //
 
 public extension Optional {
-    
-    func filter(_ predicate: @noescape (Wrapped) -> Bool) -> Optional {
 
+    func filter(_ predicate: (Wrapped) -> Bool) -> Optional {
         return map(predicate) == .some(true) ? self : .none
     }
 
-    func mapNil(_ predicate: @noescape (Void) -> Wrapped) -> Optional {
-
+    func mapNil(_ predicate: (Void) -> Wrapped) -> Optional {
         return self ?? .some(predicate())
     }
-    
-    func flatMapNil(_ predicate: @noescape (Void) -> Optional) -> Optional {
-        
+
+    func flatMapNil(_ predicate: (Void) -> Optional) -> Optional {
         return self ?? predicate()
     }
 
-    func then(_ f: @noescape (Wrapped) -> Void) {
-
+    func then(_ f: (Wrapped) -> Void) {
         if let wrapped = self { f(wrapped) }
     }
-    
-    
-    func maybe<U>(_ defaultValue: U, f: @noescape (Wrapped) -> U) -> U {
-        
+
+    func maybe<U>(_ defaultValue: U, f: (Wrapped) -> U) -> U {
         return map(f) ?? defaultValue
     }
 
-    func onSome(_ f: @noescape (Wrapped) -> Void) -> Optional {
-
+    func onSome(_ f: (Wrapped) -> Void) -> Optional {
         then(f)
         return self
     }
 
-    func onNone(_ f: @noescape (Void) -> Void) -> Optional {
-
+    func onNone(_ f: (Void) -> Void) -> Optional {
         if isNone { f() }
         return self
     }
 
     var isSome: Bool {
-
         return self != nil
     }
 
-        var isNone: Bool {
-
+    var isNone: Bool {
         return !isSome
     }
 }
